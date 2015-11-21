8898bcd255d9a82b33a5a4884afa179f038d9ce2
Frank Kotsianas <fkotsianas@pivotal.io>
2015-11-20 16:29:34 -0800

    Remove potentially time saving yet potentially erroneous clause from name validation.
    Signed-off-by: Kara Felix <kfelix@us.ibm.com>

--- app/models/services/service_instance.rb
+++ app/models/services/service_instance.rb
@@ -72,11 +72,7 @@ module VCAP::CloudController
     def validate
       validates_presence :name
       validates_presence :space
-      validates_unique [:space_id, :name], where: (proc do |_, obj, arr|
-                                                     vals = arr.map { |x| obj.send(x) }
-                                                     next if vals.any?(&:nil?)
-                                                     ServiceInstance.where(arr.zip(vals))
-                                                   end)
+      validates_unique [:space_id, :name]
       validates_max_length 50, :name
     end
 
